4bcb3c8a9d62e92b41090322b8cfe364fa298c9d
blue333 <1014464483@qq.com>
2022-01-23 17:12:20 +0800

    23th

--- JsInLoginpage/reg.js
+++ JsInLoginpage/reg.js
@@ -6,18 +6,32 @@ let answer = document.getElementById('answer');
 let regbtn = document.getElementById('regbtn');
 
 let formData = new FormData();
-
+formData.append('username', '');//上传用户名input内的值
+formData.append('password', '');//上传密码input内的值
+formData.append('question', '');//上传密码input内的值
+formData.append('answer', '');//上传密码input内的值
 
 regbtn.onclick = () => {
-    formData.append('username', RegUn.value);//上传用户名input内的值
-    formData.append('password', RegPsw.value);//上传密码input内的值
-    formData.append('question', question.value);//上传密码input内的值
-    formData.append('answer', answer.value);//上传密码input内的值
-    for (var value of formData.values()) {
-        console.log(value);
-    }
-    if (question.value == '')
-        alert("请填写密保问题!");
+    formData.set('username', RegUn.value);//上传用户名input内的值
+    formData.set('password', RegPsw.value);//上传密码input内的值
+    formData.set('question', question.value);//上传密码input内的值
+    formData.set('answer', answer.value);//上传密码input内的值
+    // for (var value of formData.values()) {
+    //     console.log(value);
+    // }
+    if (RegUn.value == '')
+        alert('用户名不能为空!')
+    else if (RegPsw.value == '')
+        alert('密码不能为空!')
+    else if (RegPsw.value.length < 8)
+        alert('密码长度过短! 密码长度应为8到16位')
+    else if (RegPsw.value.length > 16)
+        alert('密码长度过长! 密码长度应为8到16位')
+    else if (question.value == '')
+        alert("请填写密保问题!")
+    else if (answer.value == '')
+        alert("请填写密保答案!")
+
     fetch("http://121.41.120.238:8080/user/register", {
         method: 'POST',
         body: formData
@@ -25,7 +39,8 @@ regbtn.onclick = () => {
         .then(Response => Response.json())
         .then(res => {
             let a = res.info;
-            alert(a + '密码应为8到16位');
+            if(a=='注册成功!' ||a=='该账号已经被注册!')
+            alert(a);
         })
 
 }
--- JsInLoginpage/tap.js
+++ JsInLoginpage/tap.js
@@ -7,20 +7,21 @@ const line2 = document.getElementById('line2');
 const c1 = document.getElementById('c1');
 const c2 = document.getElementById('c2');
 
-
-function loginclick() {
+c1.addEventListener('click', () => {
     reg.style.display = 'none';
     login.style.display = 'block';
-    line1.style.backgroundColor='black';
-    line2.style.backgroundColor='rgb(212, 212, 212)';
-    c1.style.color='black';
-    c2.style.color='rgb(212, 212, 212)';
-}
-function regclick() {
+    line1.style.backgroundColor = 'black';
+    line2.style.backgroundColor = 'rgb(212, 212, 212)';
+    c1.style.color = 'black';
+    c2.style.color = 'rgb(212, 212, 212)';
+})
+
+c2.addEventListener('click', () => {
     reg.style.display = 'block';
     login.style.display = 'none';
-    line2.style.backgroundColor='black';
-    line1.style.backgroundColor='rgb(212, 212, 212)';
-    c2.style.color='black';
-    c1.style.color='rgb(212, 212, 212)';
-}
+    line2.style.backgroundColor = 'black';
+    line1.style.backgroundColor = 'rgb(212, 212, 212)';
+    c2.style.color = 'black';
+    c1.style.color = 'rgb(212, 212, 212)';
+})
+
