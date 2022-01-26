7765c1036cbddace2152d02142d4e92677896839
blue333 <1014464483@qq.com>
2022-01-26 17:05:28 +0800

    26th

--- JS/Homepage/LoginCheck copy.js
+++ JS/Homepage/LoginCheck copy.js
@@ -2,21 +2,23 @@ const unlog = document.getElementById('loginOrRegist');//获取红框
 const log = document.getElementById('userHome');//获取篮筐
 console.log('?')
 let tokenvalue = '';
-tokenvalue = window.localStorage.getItem('token');//获取token的值
-console.log('tokenvalue=  '+tokenvalue);//打印token的值
+tokenvalue = window.localStorage.getItem('token');//获取token
+console.log('tokenvalue='+tokenvalue);//打印token
 
 let formdata = new FormData();
 formdata.append('token','');
 
-if(tokenvalue!=''){//token存在
-    formdata.set('token','');
-    console.log('token的值不为空')
-}
+// if(tokenvalue == ''){//token为空或没有token
+//     console.log(1)
+//     console.log('token='+ token)
+// }
 
-else{
-    formdata.set('token',tokenvalue);
-    console.log(1)
-}
+// else{//token存在
+//     formdata.append('token','');
+//     formdata.set('token',tokenvalue);
+//     // console.log(tokenvalue)
+//     console.log('token=' + tokenvalue)
+// }
 
 const a = await fetch("http://121.41.120.238:8080/user/check", {
     method: 'POST',
@@ -25,7 +27,6 @@ const a = await fetch("http://121.41.120.238:8080/user/check", {
 
 const res = await a.json();
 console.log(res.info);
-let username = res.info;
 
 if (res.info == '你还没有登录！') {
     unlog.style.display = 'block';
@@ -36,5 +37,10 @@ if (res.info == '你还没有登录！') {
     unlog.style.display = 'none';
     log.style.display = 'block';
 }
+console.log('??')
+console.log('???')
 
-document.getElementById('whos').innerHTML=username+'的账号'
+// log.addEventListener('click',()=>{
+    
+// })
+
