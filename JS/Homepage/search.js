4a80ae7c015097588b285f526e59e4529a40ceb5
blue333 <1014464483@qq.com>
2022-01-30 13:42:53 +0800

    30th

--- JS/Homepage/search.js
+++ JS/Homepage/search.js
@@ -1,19 +1,17 @@
 const search = document.getElementById("search");
 const searchBt = document.getElementById('searchBt');
-let searchForm = new FormData();
-searchForm.append('name', '')
+let formdata = new FormData();
+formdata.append('name', '')
 
 searchBt.addEventListener('click', async () => {
-    searchForm.set('name', search.value);
-    for (var value of searchForm.values()) {
+    formdata.set('name', search.value);
+    for (var value of formdata.values()) {
         console.log(value);
     }
-    console.log(search)
     const mf = await fetch("http://121.41.120.238:8080/movie/find", {
         method: 'POST',
-        body: searchForm
-
+        body: formdata
     })
-    const mfres = mf.json();
+    const mfres = await mf.json();
     console.log(mfres);
 })
