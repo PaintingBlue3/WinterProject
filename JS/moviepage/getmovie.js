cdb9596cb2c4f87e8e7dec01c96f891c603f9509
blue333 <1014464483@qq.com>
2022-02-11 15:38:14 +0800

    2.11

--- JS/moviepage/getmovie.js
+++ JS/moviepage/getmovie.js
@@ -1,7 +1,12 @@
 const tt = window.localStorage.getItem('imdb');
-const title = document.querySelector('.a3')
-const cb = document.querySelector('.cover')
-console.log(title)
+const title = document.querySelector('.a3');
+const cb = document.querySelector('.cover');
+const ms = document.querySelectorAll('.mt');
+const bt = document.getElementById('bt');
+const btt = document.getElementById('btt');
+const bc = document.getElementById('bc');
+console.log(bt.innerHTML)
+
 
 //查电影名
 let ttform = new FormData();
@@ -12,6 +17,15 @@ const tts = await fetch("http://121.41.120.238:8080/movie/findByIMDB", {
 })
 const ttres = await tts.json();
 title.innerHTML = ttres.information[0].name
+bt.innerHTML = ttres.information[0].name + '的剧情简介 · · · · · ·'
+bc.innerHTML = ttres.information[0].name + '的短评 · · · · · ·'
+
+//改信息
+ms[3].innerHTML = ttres.information[0].type;
+ms[4].innerHTML = ttres.information[0].date;
+ms[5].innerHTML = ttres.information[0].long + '分钟';
+ms[6].innerHTML = ttres.information[0].alias;
+ms[7].innerHTML = ttres.information[0].imdb;
 
 
 //查封面
@@ -23,7 +37,8 @@ const cover = await fetch("http://121.41.120.238:8080/movie/findByIMDB", {
     body: coverform
 })
 const coverres = await cover.json();
-console.log(coverres.information[0].picture_1);//封面
+console.log(coverres.information[0].picture_1); //封面
+btt.innerHTML = '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp' + coverres.information[0].brief;
 
 //插入封面
 let pic = document.createElement('img');
