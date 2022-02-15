9ecee2dbdc63ae4f7df6b87224bec9477ae36118
blue333 <1014464483@qq.com>
2022-02-15 22:21:44 +0800

    2.15

---
+++ JS/moviepage/wantWatch.js
@@ -0,0 +1,7 @@
+const btn1 = document.querySelector('#wtl');
+const btn2 = document.querySelector('#hld');
+
+let wtlform = new FormData();
+const wanttolook = await fetch('http://121.41.120.238:8080/message/pointOnly', {
+
+})
